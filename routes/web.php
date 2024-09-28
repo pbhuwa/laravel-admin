4ae0461c2273bf4d397417aa9156a24f0e09989c
pbhuwa <bhuwanpariyarr@gmail.com>
2024-09-28 23:37:33 +0545

    removed commented code

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,6 @@ Route::middleware('auth')->group(function () {
 
     // User Profile
     Route::get('/profile', [ProfileController::class, 'index'])->name('profile.index');
-
-    // Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
