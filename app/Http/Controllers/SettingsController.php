3b41e27001987e190123cf13be1c53d6805275d7
pbhuwa <bhuwanpariyarr@gmail.com>
2024-09-28 23:29:40 +0545

    wrapped with double quote

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -2,12 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class SettingsController extends Controller
 {
     public function index()
     {
-        return view("settings");
+        return view('settings');
     }
 }
--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class UsersController extends Controller
@@ -10,6 +9,7 @@ class UsersController extends Controller
     public function index()
     {
         $user = Auth::user();
-        return view("profile.user", compact("user"));
+
+        return view('profile.user', compact('user'));
     }
 }
