59bd6bf0f5cf45f4c02e43e83929627f3109a183
pbhuwa <bhuwanpariyarr@gmail.com>
2024-10-07 00:35:23 +0545

    designed fixed

--- resources/views/components/primary-button.blade.php
+++ resources/views/components/primary-button.blade.php
@@ -2,7 +2,7 @@
 
 <button type="{{ $type }}" {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge([
     'class' =>
-        'inline-block rounded-md bg-primary px-4 pb-2 pt-2 text-xs font-medium uppercase leading-normal text-white shadow-primary-3 transition duration-150 ease-in-out hover:bg-primary-accent-300 hover:shadow-primary-2 focus:bg-primary-accent-300 focus:shadow-primary-2 focus:outline-none focus:ring-0 active:bg-primary-600 active:shadow-primary-2 dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong',
+        'inline-block rounded-sm bg-primary px-4 pb-2 pt-2 text-xs font-medium uppercase leading-normal text-white shadow-primary-3 transition duration-150 ease-in-out hover:bg-primary-accent-300 hover:shadow-primary-2 focus:bg-primary-accent-300 focus:shadow-primary-2 focus:outline-none focus:ring-0 active:bg-primary-600 active:shadow-primary-2 dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong',
     'data-twe-ripple-init' => 'true',
     'data-twe-ripple-color' => 'light',
 ]) !!}>
