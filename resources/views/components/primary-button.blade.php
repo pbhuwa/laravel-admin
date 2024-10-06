7d7daa522166d8d6d8a64741a07b913960add0b6
pbhuwa <bhuwanpariyarr@gmail.com>
2024-10-06 22:34:50 +0545

    designed button

--- resources/views/components/primary-button.blade.php
+++ resources/views/components/primary-button.blade.php
@@ -1,5 +1,10 @@
-<button
-    {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-block rounded !bg-primary-700 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-md transition duration-150 ease-in-out hover:!bg-primary-accent-300 focus:!bg-primary-accent-300 active:!bg-primary-600']) }}>
-    <i class="fa-solid fa-floppy-disk"></i> &nbsp;
+@props(['type' => 'submit', 'disabled' => false])
+
+<button type="{{ $type }}" {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge([
+    'class' =>
+        'inline-block rounded-md bg-primary px-4 pb-2 pt-2 text-xs font-medium uppercase leading-normal text-white shadow-primary-3 transition duration-150 ease-in-out hover:bg-primary-accent-300 hover:shadow-primary-2 focus:bg-primary-accent-300 focus:shadow-primary-2 focus:outline-none focus:ring-0 active:bg-primary-600 active:shadow-primary-2 dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong',
+    'data-twe-ripple-init' => 'true',
+    'data-twe-ripple-color' => 'light',
+]) !!}>
     {{ $slot }}
 </button>
