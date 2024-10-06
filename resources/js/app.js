9f626575e10f50fb9e12e008ac9ce7da8f7f90c0
pbhuwa <bhuwanpariyarr@gmail.com>
2024-10-06 22:32:07 +0545

    removed tw-elements package

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,1 @@
 import "./bootstrap";
-
-// Import tailwind css components from tw-elements
-import { Tooltip, Ripple, Tab, Input, initTWE } from "tw-elements";
-
-// Initialize all components
-initTWE({ Tooltip, Ripple, Tab, Input });
