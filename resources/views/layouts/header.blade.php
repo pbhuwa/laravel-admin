38b0bad5b0917f8195e4fc17b1eb1715fc672b0a
pbhuwa <bhuwanpariyarr@gmail.com>
2024-10-11 22:45:56 +0545

    reduced text size

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,14 +1,19 @@
-<div class="py-2 px-6 bg-white flex items-center shadow-md shadow-black/5 sticky top-0 left-0 z-30">
-    <a href="#" class="flex items-center mr-2">
-        <img src="https://placehold.co/32x32" alt="" class="w-8 h-8 rounded object-cover">
-    </a>
-    <button type="button" class="text-lg text-gray-600 sidebar-toggle">
+<div class="py-2 px-6 bg-cyan-950 dark:bg-cyan-950 flex items-center shadow-md shadow-black/5 sticky top-0 left-0 z-30">
+    <button type="button" class="text-lg text-gray-300 sidebar-toggle mr-3">
         <i class="fa-solid fa-bars"></i>
     </button>
+    <a href="#" class="flex items-center mr-3">
+        <img src="https://placehold.co/32x32" alt="" class="w-8 h-8 rounded object-cover">
+    </a>
+    <!-- Toggle button -->
+    <button id="sidebarToggle" class="flex text-lg text-gray-300 sidebar-toggle mr-3">
+        <i class="fa-solid fa-bars"></i>
+    </button>
+
     <ul class="ml-auto flex items-center">
         <li class="mr-1 dropdown">
             <button type="button"
-                class="dropdown-toggle text-gray-400 w-8 h-8 rounded flex items-center justify-center hover:bg-gray-50 hover:text-gray-600">
+                class="dropdown-toggle text-gray-300 w-8 h-8 rounded flex items-center justify-center hover:bg-gray-50 hover:text-gray-600">
                 <i class="fa-solid fa-magnifying-glass"></i>
             </button>
             <div
@@ -19,11 +24,11 @@
                             class="py-2 pr-4 pl-10 bg-gray-50 w-full outline-none border border-gray-100 rounded-md text-sm focus:border-blue-500"
                             placeholder="Search...">
                         <i
-                            class="fa-solid fa-magnifying-glass absolute top-1/2 left-4 -translate-y-1/2 text-gray-400"></i>
+                            class="fa-solid fa-magnifying-glass absolute top-1/2 left-4 -translate-y-1/2 text-gray-300"></i>
                     </div>
                 </form>
                 <div class="mt-3 mb-2">
-                    <div class="text-[13px] font-medium text-gray-400 ml-4 mb-2">Recently</div>
+                    <div class="text-[13px] font-medium text-gray-300 ml-4 mb-2">Recently</div>
                     <ul class="max-h-64 overflow-y-auto">
                         <li>
                             <a href="#" class="py-2 px-4 flex items-center hover:bg-gray-50 group">
@@ -33,7 +38,7 @@
                                     <div
                                         class="text-[13px] text-gray-600 font-medium truncate group-hover:text-blue-500">
                                         Create landing page</div>
-                                    <div class="text-[11px] text-gray-400">$345</div>
+                                    <div class="text-[11px] text-gray-300">$345</div>
                                 </div>
                             </a>
                         </li>
@@ -45,7 +50,7 @@
                                     <div
                                         class="text-[13px] text-gray-600 font-medium truncate group-hover:text-blue-500">
                                         Create landing page</div>
-                                    <div class="text-[11px] text-gray-400">$345</div>
+                                    <div class="text-[11px] text-gray-300">$345</div>
                                 </div>
                             </a>
                         </li>
@@ -57,7 +62,7 @@
                                     <div
                                         class="text-[13px] text-gray-600 font-medium truncate group-hover:text-blue-500">
                                         Create landing page</div>
-                                    <div class="text-[11px] text-gray-400">$345</div>
+                                    <div class="text-[11px] text-gray-300">$345</div>
                                 </div>
                             </a>
                         </li>
@@ -69,7 +74,7 @@
                                     <div
                                         class="text-[13px] text-gray-600 font-medium truncate group-hover:text-blue-500">
                                         Create landing page</div>
-                                    <div class="text-[11px] text-gray-400">$345</div>
+                                    <div class="text-[11px] text-gray-300">$345</div>
                                 </div>
                             </a>
                         </li>
@@ -81,7 +86,7 @@
                                     <div
                                         class="text-[13px] text-gray-600 font-medium truncate group-hover:text-blue-500">
                                         Create landing page</div>
-                                    <div class="text-[11px] text-gray-400">$345</div>
+                                    <div class="text-[11px] text-gray-300">$345</div>
                                 </div>
                             </a>
                         </li>
@@ -93,7 +98,7 @@
                                     <div
                                         class="text-[13px] text-gray-600 font-medium truncate group-hover:text-blue-500">
                                         Create landing page</div>
-                                    <div class="text-[11px] text-gray-400">$345</div>
+                                    <div class="text-[11px] text-gray-300">$345</div>
                                 </div>
                             </a>
                         </li>
@@ -105,7 +110,7 @@
                                     <div
                                         class="text-[13px] text-gray-600 font-medium truncate group-hover:text-blue-500">
                                         Create landing page</div>
-                                    <div class="text-[11px] text-gray-400">$345</div>
+                                    <div class="text-[11px] text-gray-300">$345</div>
                                 </div>
                             </a>
                         </li>
@@ -117,7 +122,7 @@
                                     <div
                                         class="text-[13px] text-gray-600 font-medium truncate group-hover:text-blue-500">
                                         Create landing page</div>
-                                    <div class="text-[11px] text-gray-400">$345</div>
+                                    <div class="text-[11px] text-gray-300">$345</div>
                                 </div>
                             </a>
                         </li>
@@ -127,16 +132,16 @@
         </li>
         <li class="dropdown">
             <button type="button"
-                class="dropdown-toggle text-gray-400 w-8 h-8 rounded flex items-center justify-center hover:bg-gray-50 hover:text-gray-600">
+                class="dropdown-toggle text-gray-300 w-8 h-8 rounded flex items-center justify-center hover:bg-gray-50 hover:text-gray-600">
                 <i class="fa-regular fa-bell"></i>
             </button>
             <div
                 class="dropdown-menu shadow-md shadow-black/5 z-30 hidden max-w-xs w-full bg-white rounded-md border border-gray-100">
                 <div class="flex items-center px-4 pt-4 border-b border-b-gray-100 notification-tab">
                     <button type="button" data-tab="notification" data-tab-page="notifications"
-                        class="text-gray-400 font-medium text-[13px] hover:text-gray-600 border-b-2 border-b-transparent mr-4 pb-1 active">Notifications</button>
+                        class="text-gray-300 font-medium text-[13px] hover:text-gray-600 border-b-2 border-b-transparent mr-4 pb-1 active">Notifications</button>
                     <button type="button" data-tab="notification" data-tab-page="messages"
-                        class="text-gray-400 font-medium text-[13px] hover:text-gray-600 border-b-2 border-b-transparent mr-4 pb-1">Messages</button>
+                        class="text-gray-300 font-medium text-[13px] hover:text-gray-600 border-b-2 border-b-transparent mr-4 pb-1">Messages</button>
                 </div>
                 <div class="my-2">
                     <ul class="max-h-64 overflow-y-auto" data-tab-for="notification" data-page="notifications">
@@ -148,7 +153,7 @@
                                     <div
                                         class="text-[13px] text-gray-600 font-medium truncate group-hover:text-blue-500">
                                         New order</div>
-                                    <div class="text-[11px] text-gray-400">from a user</div>
+                                    <div class="text-[11px] text-gray-300">from a user</div>
                                 </div>
                             </a>
                         </li>
@@ -160,7 +165,7 @@
                                     <div
                                         class="text-[13px] text-gray-600 font-medium truncate group-hover:text-blue-500">
                                         New order</div>
-                                    <div class="text-[11px] text-gray-400">from a user</div>
+                                    <div class="text-[11px] text-gray-300">from a user</div>
                                 </div>
                             </a>
                         </li>
@@ -172,7 +177,7 @@
                                     <div
                                         class="text-[13px] text-gray-600 font-medium truncate group-hover:text-blue-500">
                                         New order</div>
-                                    <div class="text-[11px] text-gray-400">from a user</div>
+                                    <div class="text-[11px] text-gray-300">from a user</div>
                                 </div>
                             </a>
                         </li>
@@ -184,7 +189,7 @@
                                     <div
                                         class="text-[13px] text-gray-600 font-medium truncate group-hover:text-blue-500">
                                         New order</div>
-                                    <div class="text-[11px] text-gray-400">from a user</div>
+                                    <div class="text-[11px] text-gray-300">from a user</div>
                                 </div>
                             </a>
                         </li>
@@ -196,7 +201,7 @@
                                     <div
                                         class="text-[13px] text-gray-600 font-medium truncate group-hover:text-blue-500">
                                         New order</div>
-                                    <div class="text-[11px] text-gray-400">from a user</div>
+                                    <div class="text-[11px] text-gray-300">from a user</div>
                                 </div>
                             </a>
                         </li>
@@ -210,7 +215,7 @@
                                     <div
                                         class="text-[13px] text-gray-600 font-medium truncate group-hover:text-blue-500">
                                         John Doe</div>
-                                    <div class="text-[11px] text-gray-400">Hello there!</div>
+                                    <div class="text-[11px] text-gray-300">Hello there!</div>
                                 </div>
                             </a>
                         </li>
@@ -222,7 +227,7 @@
                                     <div
                                         class="text-[13px] text-gray-600 font-medium truncate group-hover:text-blue-500">
                                         John Doe</div>
-                                    <div class="text-[11px] text-gray-400">Hello there!</div>
+                                    <div class="text-[11px] text-gray-300">Hello there!</div>
                                 </div>
                             </a>
                         </li>
@@ -234,7 +239,7 @@
                                     <div
                                         class="text-[13px] text-gray-600 font-medium truncate group-hover:text-blue-500">
                                         John Doe</div>
-                                    <div class="text-[11px] text-gray-400">Hello there!</div>
+                                    <div class="text-[11px] text-gray-300">Hello there!</div>
                                 </div>
                             </a>
                         </li>
@@ -246,7 +251,7 @@
                                     <div
                                         class="text-[13px] text-gray-600 font-medium truncate group-hover:text-blue-500">
                                         John Doe</div>
-                                    <div class="text-[11px] text-gray-400">Hello there!</div>
+                                    <div class="text-[11px] text-gray-300">Hello there!</div>
                                 </div>
                             </a>
                         </li>
@@ -258,7 +263,7 @@
                                     <div
                                         class="text-[13px] text-gray-600 font-medium truncate group-hover:text-blue-500">
                                         John Doe</div>
-                                    <div class="text-[11px] text-gray-400">Hello there!</div>
+                                    <div class="text-[11px] text-gray-300">Hello there!</div>
                                 </div>
                             </a>
                         </li>
